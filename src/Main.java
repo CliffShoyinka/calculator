f5e5f83d3be3bc96b3e0a6e9bda40557a89008b8
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-03-04 03:19:21 +0300

    march 4,2023 v3

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,10 @@ public class Main {
 
         int select;
 
-        int sum = 0;
+
+        System.out.println("Please enter first number...");
+
+        int num1 = input.nextInt();
 
         do {
 
@@ -22,17 +25,15 @@ public class Main {
                     "\n2- subtraction" +
                     "\n3- divide" +
                     "\n4- multiply " +
+                    "\n5- reset" +
                     "\n0- exit");
 
             System.out.println();
 
             select = input.nextInt();
 
-            if (select == 1 || select == 2 || select == 3 || select == 4) {
+            if (select == 1 || select == 2 || select == 3 || select == 4 || select == 5) {
 
-                System.out.println("Please enter first number...");
-
-                int num1 = input.nextInt();
 
                 System.out.println("Please enter second number...");
 
@@ -41,20 +42,22 @@ public class Main {
                 switch (select) {
 
                     case 1:
-                        sum = num1 + num2 ;
-                        System.out.println("Result: "+sum);
+                        num1 = num1 + num2 ;
+                        System.out.println("Result: "+num1);
                         break;
                     case 2:
-                        sum = num1 - num2 ;
-                        System.out.println("Result: "+sum);
+                        num1 = num1 - num2 ;
+                        System.out.println("Result: "+num1);
                         break;
                     case 3:
-                        sum = num1 / num2 ;
-                        System.out.println("Result: "+sum);
+                        num1 = num1 / num2 ;
+                        System.out.println("Result: "+num1);
                         break;
                     case 4:
-                        sum = num1 * num2;
-                        System.out.println("Result: "+sum);
+                        num1 = num1 * num2;
+                        System.out.println("Result: "+num1);
+                    case 5:
+                        num1 = 0;
                     default:
                         break;
 
